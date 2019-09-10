114d2293f5863c9c8ebe90c56da46f15afb4daee
Will Drougas <wdrougas@gmail.com>
2019-09-10 13:59:22 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,4 @@ second_number == 7
 sum = (first_number) + (second_number)
 difference = (first_number) - (second_number)
 product = (first_number) * (second_number)
-quotient = (first_number) \ (second_number)
+quotient = (first_number) / (second_number)
